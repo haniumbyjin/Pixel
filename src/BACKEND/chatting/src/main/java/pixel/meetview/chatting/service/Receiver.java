4e4afaf640d7851bb7f88a2fc66fd8860489015e
LeeYeJi <lwj1748@naver.com>
2019-07-18 14:57:10 +0900

    Add: B/E kafka

---
+++ src/BACKEND/chatting/src/main/java/pixel/meetview/chatting/service/Receiver.java
@@ -0,0 +1,34 @@
+package pixel.meetview.chatting.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+import pixel.meetview.chatting.model.ChattingMessage;
+
+import java.util.HashMap;
+
+@Service
+public class Receiver {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
+
+    @Autowired
+    private SimpMessagingTemplate template;
+
+    @KafkaListener(id = "main-listener", topics = "kafka-chatting")
+    public void receive(ChattingMessage message) throws Exception {
+        LOGGER.info("message='{}'", message);
+        HashMap<String, String> msg = new HashMap<>();
+        msg.put("timestamp", Long.toString(message.getTimeStamp()));
+        msg.put("message", message.getMessage());
+        msg.put("user", message.getUser());
+
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(msg);
+
+        this.template.convertAndSend("/topic/public", json);
+    }
+}
