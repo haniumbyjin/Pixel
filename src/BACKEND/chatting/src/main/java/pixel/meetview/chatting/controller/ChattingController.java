3483913814d910d44bbaa927875c0a3b630a35c4
sorin <jnsorn@gmail.com>
2019-07-21 14:16:34 +0900

    Update: remove @SendTo in ChattingController

--- src/BACKEND/chatting/src/main/java/pixel/meetview/chatting/controller/ChattingController.java
+++ src/BACKEND/chatting/src/main/java/pixel/meetview/chatting/controller/ChattingController.java
@@ -23,7 +23,7 @@ public class ChattingController {
 
     // "url/app/message"로 들어오는 메시지를 "/topic/public"을 구독하고있는 사람들에게 송신
     @MessageMapping("/message")//@MessageMapping works for WebSocket protocol communication. This defines the URL mapping.
-    @SendTo("/topic/public")//websocket subscribe topic& direct send
+    // @SendTo("/topic/public")//websocket subscribe topic& direct send
     public void sendMessage(ChattingMessage message) throws Exception {
         message.setTimeStamp(System.currentTimeMillis());
         chattingMessageDAO.save(message);
