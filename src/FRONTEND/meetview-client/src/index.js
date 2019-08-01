cb4add079d83a07e3a406e175c4a2fcab505b8ea
LeeYeJi <lwj1748@naver.com>
2019-08-01 16:26:51 +0900

    Add: MobX to client

--- src/FRONTEND/meetview-client/src/index.js
+++ src/FRONTEND/meetview-client/src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react'; // MobX 에서 사용하는 Provider
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import RootStore from './stores/RootStore';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+const root = new RootStore();
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Provider {...root}>
+            <App />
+        </Provider>,
+    </BrowserRouter>,
+    document.getElementById('root')
+ );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
