6319416892580b4511eb109048357805974682da
LeeYeJi <lwj1748@naver.com>
2019-08-03 19:35:30 +0900

    Add: test sample

--- src/BACKEND/chatting/src/test/java/pixel/meetview/chatting/ChattingApplicationTests.java
+++ src/BACKEND/chatting/src/test/java/pixel/meetview/chatting/ChattingApplicationTests.java
@@ -1,16 +1,52 @@
 package pixel.meetview.chatting;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import pixel.meetview.chatting.model.response.ChattingMessage;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ChattingApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void test() throws Exception {
+        ChattingMessage message = ChattingMessage.builder().content("hello").build();
+
+        String json = asJsonString(message);
+
+        mockMvc.perform(post("/test")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)) // content의 type을 명시합니다.
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    public String asJsonString(Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            return mapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
