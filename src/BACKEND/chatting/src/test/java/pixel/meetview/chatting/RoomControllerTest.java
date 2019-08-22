9cab9446d769e0ba590d1bd7ab588802b8ef720c
LeeYeJi <lwj1748@naver.com>
2019-08-22 16:52:34 +0900

    Add: test case

--- src/BACKEND/chatting/src/test/java/pixel/meetview/chatting/RoomControllerTest.java
+++ src/BACKEND/chatting/src/test/java/pixel/meetview/chatting/RoomControllerTest.java
@@ -9,7 +9,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import pixel.meetview.chatting.model.response.ChattingRoom;
 import pixel.meetview.chatting.model.response.User;
+
+import java.util.ArrayList;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -25,12 +28,26 @@ public class RoomControllerTest {
 
     @Test
     public void createRoomTest() throws Exception {
+        ArrayList<User> users = new ArrayList<>();
         User user = User.builder()
-                .user_uid(0)
                 .user_id("test_id")
                 .build();
+        User user2 = User.builder()
+                .user_id("test_id2")
+                .build();
+        users.add(user);
+        users.add(user2); // client에서는 이렇게 넘어가겠지만 List에는 JsonIgnore를 해놓아서 test불가
 
-        String json = asJsonString(user);
+        //채팅방 참여자목록
+
+        ChattingRoom room = ChattingRoom.builder()
+                .room_name("test room")
+                .joinUsers(users)
+                .master_id(user.getUser_id())
+                .build();
+
+        String json = asJsonString(room);
+        System.out.println("json string: "+json);
 
         mockMvc.perform(post("/createroom")
                 .content(json)
